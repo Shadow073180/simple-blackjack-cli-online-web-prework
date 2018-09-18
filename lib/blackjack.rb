77d5581359764a99a4417996a7fcbc62a86b88c2
David masterson <david73180@gmail.com>
2018-09-18 02:12:04 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,80 @@
+require 'pry'
+
+
+
 def welcome
-  # code #welcome here
+ puts ("Welcome to the Blackjack Table")
 end
+
+
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
+  
 end
 
-def display_card_total
-  # code #display_card_total here
+
+def display_card_total(sum)
+  puts ("Your cards add up to #{sum}")
+  
 end
+ 
 
 def prompt_user
-  # code #prompt_user here
+  puts ("Type 'h' to hit or 's' to stay")
 end
+ 
 
 def get_user_input
-  # code #get_user_input here
+input = gets.chomp
+return input
+  
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts ("Sorry, you hit #{total}. Thanks for playing!")
 end
+
 
 def initial_round
-  # code #initial_round here
+  sum = deal_card
+  sum = sum + deal_card
+  display_card_total(sum)
+  return sum
 end
+
+
+
 
 def hit?
-  # code hit? here
+  prompt_user 
+ sum = get_user_input(sum)
+  if get_user_input == 's'
+    return sum
+  elsif get_user_input == 'h'
+   sum = sum + deal_card
+  else 
+    invalid_command
+  end
+  
 end
 
+
 def invalid_command
-  # code invalid_command here
+  puts "That was an invalid selection."
 end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
-  # code runner here
+welcome #introduce the game
+  sleep 0.75
+  card_total = initial_round #determine your current total & tell user what number they currently have
+  until card_total > 21 #continue game until user loses
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total) #prints out once user has lost
 end
-    
+
